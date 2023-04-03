8e73ec2a1f3a16fbedafc7857c32b57d7da02493
igorsctt <igor.ss2020@hotmail.com>
2023-04-03 09:15:26 -0300

    DesafioNairuz

---
+++ javascript.js
@@ -0,0 +1,41 @@
+function aparecerCartao() {
+  let valorDoCartao = document.getElementById("inputTexto").value;
+  document.getElementById("textoDigitado").textContent = valorDoCartao;
+}
+
+document.getElementById("inputTexto").addEventListener("input", aparecerCartao);
+
+function aparecerNumero() {
+  var valor = document.getElementById("inputNumero").value;
+  var cardNumber = valor.replace(/\D/g, "");
+
+  var formattedCardNumber = "";
+  for (var i = 0; i < cardNumber.length && i < 16; i++) {
+    if (i > 0 && i % 4 == 0) {
+      formattedCardNumber += " ";
+    }
+    formattedCardNumber += cardNumber.charAt(i);
+  }
+
+  document.getElementById("textoDigitado2").textContent = formattedCardNumber;
+}
+
+document
+  .getElementById("inputNumero")
+  .addEventListener("input", aparecerNumero);
+
+function aparecerMes() {
+  let valorMes = document.getElementById("inputMm").value;
+
+  if (valorMes.length === 2) {
+    valorMes += "/";
+    document.getElementById("inputMm").value = valorMes;
+  }
+  if (valorMes.length > 3) {
+    document.getElementById("inputMm").value = valorMes;
+  }
+
+  document.getElementById("textoDigitado3").textContent = valorMes;
+}
+
+document.getElementById("inputMm").addEventListener("input", aparecerMes);
